ac56f5233a04468fae75fa1119cdae59dff042e2
David Almonacid <david.almonacidf@gmail.com>
2024-04-29 00:40:04 -0500

    refactor: Update custom form screen layout and app bar

--- lib/src/widgets/custom_form_screen.dart
+++ lib/src/widgets/custom_form_screen.dart
@@ -17,41 +17,43 @@ class CustomFormScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("")),
-      body: Stack(
-        // padding: const EdgeInsets.all(20.0),
-        children: <Widget>[
-          Column(
-            children: [
-              const SizedBox(height: 60),
-              Text(
-                title,
-                style: const TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.w700,
+      appBar: AppBar(title: const Text('')),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 32),
+        child: Stack(
+          children: <Widget>[
+            Column(
+              children: [
+                const SizedBox(height: 60),
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 120),
-              ...formFields,
-            ],
-          ),
-          Positioned(
-            bottom: 44,
-            child: ElevatedButton(
-              onPressed: onActionButtonPressed,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.cyan[900],
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(
-                  fontFamily: 'RedHatDisplay',
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              child: Text(actionButtonText),
+                const SizedBox(height: 120),
+                ...formFields,
+              ],
             ),
-          ),
-        ],
+            Positioned(
+              bottom: 44,
+              child: ElevatedButton(
+                onPressed: onActionButtonPressed,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.cyan[900],
+                  foregroundColor: Colors.white,
+                  textStyle: const TextStyle(
+                    fontFamily: 'RedHatDisplay',
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                child: Text(actionButtonText),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
