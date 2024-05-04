269ea927cea5ee80baada8345c22dd6842ce31f6
David Almonacid <david.almonacidf@gmail.com>
2024-05-04 11:42:49 -0500

    Refactor selection_categories_screen to use ListView instead of GridView for category items

--- lib/src/screens/selection_categories_screen.dart
+++ lib/src/screens/selection_categories_screen.dart
@@ -10,8 +10,6 @@ class SelectionCategoriesScreen extends StatefulWidget {
 }
 
 class _SelectionCategoriesScreenState extends State<SelectionCategoriesScreen> {
-  // final FirebaseAuth _auth = FirebaseAuth.instance;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,31 +25,55 @@ class _SelectionCategoriesScreenState extends State<SelectionCategoriesScreen> {
                   ElevatedButton(
                     onPressed: () async {
                       await FirebaseAuth.instance.signOut();
-
-                      // Return to the main screen
                       Navigator.pushReplacementNamed(context, '/');
                     },
-                    child: const Text('Cerrar sesión'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.cyan[300],
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 8,
+                      ),
+                    ),
+                    child: const Text(
+                      'Cerrar sesión',
+                      style: TextStyle(color: Color(0xFF2B3F4E)),
+                    ),
                   ),
                 ],
               ),
             ),
-            const Text('Selecciona una categoría',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            const Text(
+              'Selecciona una categoría',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 40),
             Expanded(
-              child: GridView.count(
-                crossAxisCount: 2,
+              child: ListView(
                 children: <Widget>[
                   _buildCategoryItem(
-                      'Aseo', 'assets/images/aseo.jpg', '/products_aseo'),
+                    'Aseo',
+                    'assets/images/aseo.jpg',
+                    '/products_aseo',
+                  ),
+                  const SizedBox(height: 20),
                   _buildCategoryItem(
-                      'Electrodomesticos',
-                      'assets/images/electrodomesticos.jpg',
-                      '/products_eletrodomesticos'),
-                  _buildCategoryItem('Muebles', 'assets/images/muebles.png',
-                      '/products_muebles'),
-                  _buildCategoryItem('Mercado', 'assets/images/mercado.jpg',
-                      '/products_mercado'),
+                    'Electrodomésticos',
+                    'assets/images/electrodomesticos.jpg',
+                    '/products_eletrodomesticos',
+                  ),
+                  const SizedBox(height: 20),
+                  _buildCategoryItem(
+                    'Muebles',
+                    'assets/images/muebles.png',
+                    '/products_muebles',
+                  ),
+                  const SizedBox(height: 20),
+                  _buildCategoryItem(
+                    'Mercado',
+                    'assets/images/mercado.jpg',
+                    '/products_mercado',
+                  ),
+                  const SizedBox(height: 40),
                 ],
               ),
             ),
@@ -64,11 +86,25 @@ class _SelectionCategoriesScreenState extends State<SelectionCategoriesScreen> {
   Widget _buildCategoryItem(String title, String imagePath, String routeName) {
     return GestureDetector(
       onTap: () => Navigator.pushNamed(context, routeName),
-      child: Column(
-        children: <Widget>[
-          Image.asset(imagePath, width: 172, height: 154, fit: BoxFit.fill),
-          Text(title, style: const TextStyle(fontSize: 24)),
-        ],
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(maxWidth: 200),
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 24),
+          padding: const EdgeInsets.all(12),
+          decoration: BoxDecoration(
+            color: Colors.grey[300],
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Column(
+            children: <Widget>[
+              Image.asset(imagePath, width: 172, height: 154, fit: BoxFit.fill),
+              Text(
+                title,
+                style: const TextStyle(fontSize: 18),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
